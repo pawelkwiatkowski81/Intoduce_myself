789bd0e514da1435e86542c6f66ba66007e38126
Pawel Kwiatkowski <pawelkwiatkowski81@gmail.com>
2023-07-25 19:44:29 +0200

    AppBar color change

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(
             title: const Text('Introduce myself'),
+            backgroundColor: Colors.indigoAccent,
           ),
           backgroundColor: Colors.deepOrangeAccent,
           body: Center(
